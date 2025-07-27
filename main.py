606a3e141f1e610942f7651c6e3df3688b8d9fd0
Vilde Sandegren <vildesandegren@Vilde-sin-MacBook-Air.local>
2025-07-27 17:11:38 +0200

    Removed hard code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from analysis import analyze_sequence_file
 
 def main():
-    #input_file = input("Input FASTA sequence file:")
-    input_file = "example_FASTA.txt" #hardcode for testing. Skal fjernes. 
+    input_file = input("Input FASTA sequence file:")
     save_y_n = input("Would you like to automatically save the plots created? (y/n)")
     analyze_sequence_file(input_file, save_y_n)
 
